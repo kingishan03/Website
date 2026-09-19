d6f05efc7119587693fba4fbf9ba4b1a900a9e00
Nobody <dwivedishalini0505@gmail.com>
2026-09-19 04:44:20 +0000

    1

--- Flask Web Tutorial/website/__init__.py
+++ Flask Web Tutorial/website/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask  
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjnskjdhjs'
 
-    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='')
+
     return app
